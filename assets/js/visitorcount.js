0be94fd9de7eaf8eb6a2d1fd6d5c03081025572b
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-05-14 19:37:46 +0100

    visitorcount update

--- assets/js/visitorcount.js
+++ assets/js/visitorcount.js
@@ -3,7 +3,6 @@ async function get_visitors() {
   // call post api request function
   //await post_visitor();
   try {
-    //      let response = await fetch('https://cv6b7x3ici.execute-api.eu-west-2.amazonaws.com/default/VisitorCounter', {
     let response = await fetch(
       "https://wnzlilnzu6.execute-api.eu-west-2.amazonaws.com/default/terraform_lambda_func",
       {
