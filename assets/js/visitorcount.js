82b1cb751ec6ff64053ad9d935286d135c58c9b3
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-05-14 10:20:57 +0100

    API Gateway and Lambda function Terraform IaC

--- assets/js/visitorcount.js
+++ assets/js/visitorcount.js
@@ -1,19 +1,22 @@
 // GET API REQUEST
 async function get_visitors() {
-    // call post api request function
-    //await post_visitor();
-    try {
-        let response = await fetch('https://cv6b7x3ici.execute-api.eu-west-2.amazonaws.com/default/VisitorCounter', {
-            method: 'GET',
-        });
-        let data = await response.json()
-        document.getElementById("visitors").innerHTML = data['count'];
-        console.log(data);
-        return data;
-    } catch (err) {
-        console.error(err);
-    }
+  // call post api request function
+  //await post_visitor();
+  try {
+    //      let response = await fetch('https://cv6b7x3ici.execute-api.eu-west-2.amazonaws.com/default/VisitorCounter', {
+    let response = await fetch(
+      "https://ehnsq8be98.execute-api.eu-west-2.amazonaws.com/default/terraform_lambda_func",
+      {
+        method: "GET",
+      }
+    );
+    let data = await response.json();
+    document.getElementById("visitors").innerHTML = data["count"];
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
-
 
 get_visitors();
--- infra/main.tf
+++ infra/main.tf
@@ -84,3 +84,66 @@ resource "aws_lambda_function" "terraform_lambda_func" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name              = "visitor_count_log_group"
+  retention_in_days = 30
+}
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "visitor_count_CRC"
+  protocol_type = "HTTP"
+  description = "Visitor count for Cloud Resume Challenge"
+  cors_configuration {
+    allow_origins = ["https://estebanmoreno.link", "https://www.estebanmoreno.link"]
+  }
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "default"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "terraform_lambda_func" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.terraform_lambda_func.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "terraform_lambda_func" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "ANY /terraform_lambda_func"
+  target    = "integrations/${aws_apigatewayv2_integration.terraform_lambda_func.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
