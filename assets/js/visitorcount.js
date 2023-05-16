0bef2258df205f0017522b662d09c056a455510a
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-05-16 20:04:12 +0100

    API Gateway URL Update

--- assets/js/visitorcount.js
+++ assets/js/visitorcount.js
@@ -4,7 +4,7 @@ async function get_visitors() {
   //await post_visitor();
   try {
     let response = await fetch(
-      "https://qfky693v2f.execute-api.eu-west-2.amazonaws.com/default/terraform_lambda_func",
+      "https://qfky693v2f.execute-api.eu-west-2.amazonaws.com/default/terraform_lambda_func", // API Gateway URL
       {
         method: "GET",
       }
