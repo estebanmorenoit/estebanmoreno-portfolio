aad5b59afcf823f8aa7a8350d1982354c23733c0
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-09-30 18:30:26 +0100

    amended main.tf format

--- infra/main.tf
+++ infra/main.tf
@@ -163,9 +163,9 @@ resource "aws_lambda_permission" "api_gw" {
 
 # DynamoDB Table
 resource "aws_dynamodb_table" "visitor_count_ddb" {
-  name           = "visitor_count_ddb"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  name         = "visitor_count_ddb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
